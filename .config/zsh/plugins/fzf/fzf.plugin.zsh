1271cd86e2a0935e194ecd26d4ae99d1f20b3259
Andrei Grechkin <andrei.grechkin@booking.com>
2019-11-05 17:54:36 +0100

    update fzf settings

--- .config/zsh/plugins/fzf/fzf.plugin.zsh
+++ .config/zsh/plugins/fzf/fzf.plugin.zsh
@@ -12,18 +12,22 @@ do
 	source-file "$F" && break
 done
 
+local EXCLUDE_FOLDERS_FD=(".cache" ".git" ".m2" ".vim" "git" "tmp")
+local EXCLUDE_FOLDERS_FD_STR=$(printf ' --exclude=%s' "${EXCLUDE_FOLDERS_FD[@]}")
+
 # local  GIT_LIST_FILES="git ls-files -co --recurse-submodules --exclude-standard"
 local  GIT_LIST_FILES="git ls-files -co --exclude-standard"
-local         FD_LIST="fd -HL --exclude=.git"
+local         FD_LIST="fd -HL $EXCLUDE_FOLDERS_FD_STR"
 local FIND_LIST_FILES="find -type f | grep -v '/\.git'"
 
 export FZF_DEFAULT_COMMAND="(${GIT_LIST_FILES} || ${FD_LIST} --type f || ${FIND_LIST_FILES}) 2>/dev/null"
 #export FZF_DEFAULT_COMMAND="(git ls-tree -r --name-only HEAD) 2> /dev/null"
-export FZF_DEFAULT_OPTS="--preview '(bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -40'"
+#export FZF_DEFAULT_OPTS="--preview '(bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -40'"
 
 export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
-export FZF_CTRL_T_OPTS="--preview '(bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -40' \
-	--bind 'ctrl-v:execute(vim {}),ctrl-y:execute-silent(echo {} | xclip -i -sel p -f | xclip -i -sel c)+abort'"
+export FZF_CTRL_T_OPTS="--bind 'f4:execute(vim {} < /dev/tty > /dev/tty 2>&1)' \
+	--bind 'ctrl-y:execute-silent(echo {} | xclip -i -sel p -f | xclip -i -sel c)+abort'"
+#	--preview '(bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -40'"
 
 export FZF_ALT_C_COMMAND="(${FD_LIST} --type d || find -type d | grep -v '/\.git') 2>/dev/null"
 
