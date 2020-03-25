e891fdbd65b46525f3d87c8d4e4372dc428f7197
Andrei Grechkin <andrei.grechkin@booking.com>
2020-03-25 13:42:27 +0100

    update zsh perl plugin

--- .config/zsh/plugins/perl/perl.plugin.zsh
+++ .config/zsh/plugins/perl/perl.plugin.zsh
@@ -12,33 +12,18 @@ alias perlverbose='export PERL5OPT="-MCarp=verbose"'
 
 # => functions --------------------------------------------------------------------------------------------------- {{{1
 
-function export-perl5lib() {
-	_appendvar_head PERL5LIB "$HOME/git/booking/pakket/lib"
-	_appendvar_head PERL5LIB "./lib"
-	export PERL5LIB
-}
-
-function activate-local-perl() {
-	local PERL_LOCAL=${1:-${HOME}/.local/usr}
-	if [[ ":$PERL5LIB:" != *":$PERL_LOCAL/lib/perl5:"* ]] ;then
-		[[ -e "$PERL_LOCAL/lib/perl5/local/lib.pm" ]] || echo "Preparing Perl local environment in: $PERL_LOCAL"
-		[[ -e "$PERL_LOCAL/lib/perl5/local/lib.pm" ]] || cpanm -nqf --local-lib="$PERL_LOCAL" local::lib
-		eval "$(perl -I "$PERL_LOCAL/lib/perl5/" -Mlocal::lib="$PERL_LOCAL")"
-	fi
-}
-
 function enable-perlbrew() {
-	PERLBREW_ROOT=${PERLBREW_ROOT:-${XDG_CACHE_HOME}/perlbrew}
-
 	if [[ ! -d "$PERLBREW_ROOT" ]]; then
 		NEED_PERLBREW_INIT=1
 		perlbrew init
 		perlbrew install-cpanm
 	fi
 
-	[[ -d "$PERLBREW_ROOT/perls/system/bin"      ]] || mkdir -p "$PERLBREW_ROOT/perls/system/bin"
+	# make system perl available for perlbrew
+	[[ -d "$PERLBREW_ROOT/perls/system/bin"      ]] || mkdir -p            "$PERLBREW_ROOT/perls/system/bin"
 	[[ -h "$PERLBREW_ROOT/perls/system/bin/perl" ]] || ln -s /usr/bin/perl "$PERLBREW_ROOT/perls/system/bin/perl"
 
+	# enable perlbrew environment
 	source-file "$PERLBREW_ROOT/etc/bashrc"
 	source-file "$PERLBREW_ROOT/etc/perlbrew-completion.bash"
 
@@ -48,12 +33,29 @@ function enable-perlbrew() {
 	fi
 }
 
+function activate-local-perl() {
+	local PERL_LOCAL=${1:-$HOME/.local/usr}
+	if [[ ":$PERL5LIB:" != *":$PERL_LOCAL/lib/perl5:"* ]]; then
+		# install local::lib if necessary
+		[[ -e "$PERL_LOCAL/lib/perl5/local/lib.pm" ]] || echo "Preparing Perl local environment in: $PERL_LOCAL"
+		[[ -e "$PERL_LOCAL/lib/perl5/local/lib.pm" ]] || cpanm -nqf --local-lib="$PERL_LOCAL" local::lib
+
+		# enable local::lib environment
+		eval "$(perl -I "$PERL_LOCAL/lib/perl5/" -Mlocal::lib="$PERL_LOCAL")"
+	fi
+}
+
+function export-perl5lib() {
+	_appendvar_head PERL5LIB "$HOME/git/booking/pakket/lib"
+	_appendvar_head PERL5LIB "./lib"
+	export PERL5LIB
+}
+
 # => main -------------------------------------------------------------------------------------------------------- {{{1
 
-if [[ -n "$(command -v perlbrew)" ]]; then
+if [[ -n "$(command -v perlbrew)" ]]; then                                     # perlbrew is the preferred way of managing perl and libraries
 	enable-perlbrew
-else
-	#perlbrew list 2>/dev/null | grep '\* system' &>/dev/null && activate-local-perl "$PERLBREW_ROOT/perls/system"
+else                                                                           # fallback onto local::lib if perlbrew is not available but cpanm is
 	[[ -n "$(command -v cpanm)" ]] && activate-local-perl
 fi
 
