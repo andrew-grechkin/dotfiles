3bab53914d08315094a0ae969e606dfd98c40122
Andrew Grechkin <andrew.grechkin@gmail.com>
2020-10-27 09:09:29 +0100

    update configs

--- .config/zsh/plugins/rsync/rsync.plugin.zsh
+++ .config/zsh/plugins/rsync/rsync.plugin.zsh
@@ -1,5 +1,7 @@
-alias               cpv='rsync -Pa  --sparse'
-alias        rsync-copy='rsync -ha  --sparse --info=progress2'
-alias        rsync-move='rsync -ha  --sparse --info=progress2 --remove-source-files'
-alias      rsync-update='rsync -hau --sparse --info=progress2'
-alias rsync-synchronize='rsync -hau --sparse --info=progress2 --delete'
+alias               cpv='rsync -Pa     --sparse'
+alias        rsync-copy='rsync -ha     --sparse --info=progress2'
+alias        rsync-move='rsync -ha     --sparse --info=progress2 --remove-source-files'
+alias      rsync-update='rsync -hau    --sparse --info=progress2'
+alias rsync-synchronize='rsync -hauHXx --sparse --info=progress2 --delete'
+
+alias    rsync-copy-dep='rsync -ha     --progress --exclude '@eaDir''
