8684a4d1d5cb8592b5a4e2be91f2334552111e6a
Andrew Grechkin <andrew.grechkin@gmail.com>
2020-11-13 09:02:56 +0100

    update i3 config

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ ln -sf "$WORKDIR/.xprofile"                                            ~/
 ln -sf "$WORKDIR/.config/perltidyrc"                                   ~/.perltidyrc
 
 ln -sf "$WORKDIR/.config/alacritty"                                    ~/.config/
-ln -sf "$WORKDIR/.config/compton.conf"                                 ~/.config/
+ln -sf "$WORKDIR/.config/picom.conf"                                   ~/.config/
 ln -sf "$WORKDIR/.config/fontconfig/conf.d"                            ~/.config/fontconfig/
 ln -sf "$WORKDIR/.config/git"                                          ~/.config/
 ln -sf "$WORKDIR/.config/i3"                                           ~/.config/
