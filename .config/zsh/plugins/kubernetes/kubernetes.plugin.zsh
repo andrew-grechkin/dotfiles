933031202658e8238b346e69d799f3ae1a72193a
Andrei Grechkin <andrei.grechkin@booking.com>
2021-06-28 09:28:50 +0200

    update kubectl helpers

--- .config/zsh/plugins/kubernetes/kubernetes.plugin.zsh
+++ .config/zsh/plugins/kubernetes/kubernetes.plugin.zsh
@@ -9,13 +9,13 @@ alias kaf='kubectl apply -f'
 # => aliases ----------------------------------------------------------------------------------------------------- {{{1
 
 function kabort() {
-	kubectl patch -p '{"spec":{"targetStep":0}}' --type="merge" "release.shipper.booking.com/$1"
+	kubectl --cluster "$(kubectl-management-cluster)" patch -p '{"spec":{"targetStep":0}}' --type="merge" "release.shipper.booking.com/$1"
 }
 
 function ksync() {
-	kubectl patch -p '{"spec":{"targetStep":1}}' --type="merge" "release.shipper.booking.com/$1"
+	kubectl --cluster "$(kubectl-management-cluster)" patch -p '{"spec":{"targetStep":1}}' --type="merge" "release.shipper.booking.com/$1"
 }
 
 function kfinish() {
-	kubectl patch -p '{"spec":{"targetStep":2}}' --type="merge" "release.shipper.booking.com/$1"
+	kubectl --cluster "$(kubectl-management-cluster)" patch -p '{"spec":{"targetStep":2}}' --type="merge" "release.shipper.booking.com/$1"
 }
