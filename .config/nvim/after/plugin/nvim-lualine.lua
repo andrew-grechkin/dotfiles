5f077e2d476c4723f6b9da9a8751bda1d15365ad
Andrew Grechkin <andrew.grechkin@gmail.com>
2021-10-30 11:58:47 +0200

    update configs

--- .config/nvim/after/plugin/nvim-lualine.lua
+++ .config/nvim/after/plugin/nvim-lualine.lua
@@ -4,7 +4,7 @@ if (not status) then return end
 lualine.setup {
 	options = {
 		icons_enabled = true,
-		theme = 'modus_vivendi',
+		theme = 'modus-vivendi',
 		section_separators = '',
 		component_separators = '',
 		disabled_filetypes = {}
