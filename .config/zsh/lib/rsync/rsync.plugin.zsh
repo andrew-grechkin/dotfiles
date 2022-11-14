62099e8bd62087046353a454ff902f7d6a484fbe
Andrew Grechkin <andrew.grechkin@gmail.com>
2022-11-15 00:11:23 +0100

    update configs

--- .config/zsh/lib/rsync/rsync.plugin.zsh
+++ .config/zsh/lib/rsync/rsync.plugin.zsh
@@ -1,8 +1,12 @@
-alias               cpr='rsync -PHah    --sparse --exclude "@eaDir" --exclude "#recycle"'
-alias               mvr='rsync -PHah    --sparse --exclude "@eaDir" --exclude "#recycle" --remove-source-files'
-alias               upr='rsync -PHahu   --sparse --exclude "@eaDir" --exclude "#recycle"'
+COMMON_PARAMS='--sparse --exclude "@eaDir" --exclude "#recycle" --exclude ".Trash-*"'
 
-alias        rsync-copy='rsync -Hah     --sparse --info=progress2'
-alias        rsync-move='rsync -Hah     --sparse --info=progress2 --remove-source-files'
-alias      rsync-update='rsync -Hahu    --sparse --info=progress2'
-alias rsync-synchronize='rsync -HahuAXx --sparse --info=progress2 --delete'
+alias               cpr="rsync ${COMMON_PARAMS} -PHah"
+alias               mvr="rsync ${COMMON_PARAMS} -PHah --remove-source-files"
+alias               upr="rsync ${COMMON_PARAMS} -PHahu"
+
+alias        rsync-copy="rsync ${COMMON_PARAMS} --info=progress2 -Hah"
+alias        rsync-move="rsync ${COMMON_PARAMS} --info=progress2 -Hah --remove-source-files"
+alias      rsync-update="rsync ${COMMON_PARAMS} --info=progress2 -Hahu"
+alias rsync-synchronize="rsync ${COMMON_PARAMS} --info=progress2 -HahuXx --delete"
+
+unset COMMON_PARAMS
