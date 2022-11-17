7b8caba3d261662067113de74a98605123afaf6e
Andrew Grechkin <andrew.grechkin@gmail.com>
2022-11-17 22:23:39 +0100

    update configs

--- .config/zsh/lib/rsync/rsync.plugin.zsh
+++ .config/zsh/lib/rsync/rsync.plugin.zsh
@@ -8,5 +8,6 @@ alias        rsync-copy="rsync ${COMMON_PARAMS} --info=progress2 -Hah"
 alias        rsync-move="rsync ${COMMON_PARAMS} --info=progress2 -Hah --remove-source-files"
 alias      rsync-update="rsync ${COMMON_PARAMS} --info=progress2 -Hahu"
 alias rsync-synchronize="rsync ${COMMON_PARAMS} --info=progress2 -HahuXx --delete"
+alias        rsync-diff="rsync ${COMMON_PARAMS} --dry-run -i     -HahuXx --delete"
 
 unset COMMON_PARAMS
