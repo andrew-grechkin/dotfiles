fa533a3531273ec72a27f570032373d5b33d2405
Andrei Grechkin <andrei.grechkin@booking.com>
2022-12-30 11:27:47 +0100

    update vim configs

--- .config/nvim/after/plugin/nvim-lspconfig.lua
+++ .config/nvim/after/plugin/nvim-lspconfig.lua
@@ -43,7 +43,7 @@ if status then
     config.setup {
         ensure_installed = {
             'ansiblels', 'bashls', 'cmake', 'dockerls', 'sumneko_lua', 'marksman', 'perlnavigator',
-            'puppet', 'pyright', 'sqlls',
+            'puppet', 'pyright', 'sqlls', 'tsserver',
         },
         -- 'flux_lsp',
         automatic_installation = true,
