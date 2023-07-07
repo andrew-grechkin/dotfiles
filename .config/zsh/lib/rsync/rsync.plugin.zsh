2f5c372e72b4402ed550279b97161ad5f5539de0
Andrew Grechkin <andrew.grechkin@gmail.com>
2023-07-07 18:07:19 +0200

    update configs

--- .config/zsh/lib/rsync/rsync.plugin.zsh
+++ .config/zsh/lib/rsync/rsync.plugin.zsh
@@ -1,16 +1,17 @@
-COMMON_PARAMS='--sparse --exclude="@eaDir" --exclude="#recycle" --exclude="#snapshot" --exclude=".Trash-*"'
+COMMON_PARAMS='--sparse --exclude="@eaDir" --exclude="#recycle" --exclude="#snapshot" --exclude=".Trash-*" -Hah'
 
-alias               cpr="rsync ${COMMON_PARAMS} -P -Hah"
-alias               mvr="rsync ${COMMON_PARAMS} -P -Hah --remove-source-files"
-alias               upr="rsync ${COMMON_PARAMS} -P -Hahu"
+alias               cpr="rsync ${COMMON_PARAMS} -P"
+alias               mvr="rsync ${COMMON_PARAMS} -P --remove-source-files"
+alias               upr="rsync ${COMMON_PARAMS} -P -u"
 
-alias        rsync-copy="rsync ${COMMON_PARAMS} --info=progress2 -Hah"
-alias        rsync-move="rsync ${COMMON_PARAMS} --info=progress2 -Hah --remove-source-files"
-alias      rsync-update="rsync ${COMMON_PARAMS} --info=progress2 -Hahu"
-alias rsync-synchronize="rsync ${COMMON_PARAMS} --info=progress2 -HahuXx --delete"
-alias        rsync-diff="rsync ${COMMON_PARAMS} --dry-run -i     -HahuXx --delete"
+alias        rsync-copy="rsync ${COMMON_PARAMS} --info=progress2"
+alias        rsync-move="rsync ${COMMON_PARAMS} --info=progress2 --remove-source-files"
+alias      rsync-update="rsync ${COMMON_PARAMS} --info=progress2 -u"
+alias rsync-synchronize="rsync ${COMMON_PARAMS} --info=progress2 -uXx --delete"
+alias        rsync-diff="rsync ${COMMON_PARAMS} --dry-run -i     -uXx --delete"
 
-alias     rsync-sudo-copy="sudo -E -s rsync ${COMMON_PARAMS} --info=progress2 -e 'ssh -l $USER' -Hah"
-alias   rsync-sudo-update="sudo -E -s rsync ${COMMON_PARAMS} --info=progress2 -e 'ssh -l $USER' -Hahu"
+alias     rsync-sudo-copy="sudo -E -s rsync ${COMMON_PARAMS} --info=progress2 -e 'ssh -l $USER'"
+alias     rsync-sudo-sync="sudo -E -s rsync ${COMMON_PARAMS} --info=progress2 -e 'ssh -l $USER' -uXx --delete"
+alias   rsync-sudo-update="sudo -E -s rsync ${COMMON_PARAMS} --info=progress2 -e 'ssh -l $USER' -u"
 
 unset COMMON_PARAMS
