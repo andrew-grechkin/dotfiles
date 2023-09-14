1efb8de464d326b99aa977649efc09e594654b30
Andrew Grechkin <andrew.grechkin@gmail.com>
2023-09-14 22:30:17 +0200

    update scripts and configs

--- .config/zsh/lib/kubernetes/kubernetes.plugin.zsh
+++ .config/zsh/lib/kubernetes/kubernetes.plugin.zsh
@@ -1,7 +1,7 @@
 # vim: foldmethod=marker
 # https://kubernetes.io/docs/reference/kubectl/cheatsheet/
 
-# => aliases ----------------------------------------------------------------------------------------------------- {{{1
+# => aliases ------------------------------------------------------------------------------------------------------ {{{1
 
 alias   k='kubectl'
 alias kga='kubectl get all'
@@ -9,19 +9,7 @@ alias kgp='kubectl get pods'
 alias kaf='kubectl apply -f'
 alias kevents='kubectl get events --sort-by=.metadata.creationTimestamp'
 
-# => aliases ----------------------------------------------------------------------------------------------------- {{{1
-
-function kabort() {
-	kubectl --cluster "$(kubectl-management-cluster)" patch -p '{"spec":{"targetStep":0}}' --type="merge" "release.shipper.booking.com/$1"
-}
-
-function ksync() {
-	kubectl --cluster "$(kubectl-management-cluster)" patch -p '{"spec":{"targetStep":1}}' --type="merge" "release.shipper.booking.com/$1"
-}
-
-function kfinish() {
-	kubectl --cluster "$(kubectl-management-cluster)" patch -p '{"spec":{"targetStep":2}}' --type="merge" "release.shipper.booking.com/$1"
-}
+# => aliases ------------------------------------------------------------------------------------------------------ {{{1
 
 function kgetimages() {
 	kubectl get pod -l app="$1" -o json | jq '.items[].spec.containers[] | select(.name | contains("app")) | .image'
