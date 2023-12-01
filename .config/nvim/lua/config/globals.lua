f43c4d6022fd5aa56e1d9b1101696ced9e0e463d
Andrew Grechkin <andrew.grechkin@gmail.com>
2023-12-01 21:48:00 +0100

    update scripts and configs

--- .config/nvim/lua/config/globals.lua
+++ .config/nvim/lua/config/globals.lua
@@ -5,4 +5,5 @@ IS_WORK = GIT_NAME and GIT_NAME == 'Andrei Grechkin'
 HOSTNAME = vim.fn.system({'hostname'})
 IS_KVM = not not string.find(HOSTNAME, 'king.com')
 PRIVATE_DOMAIN = 'boo' .. 'king'
+-- SUPERUSER = string.gsub(vim.fn.system('sh -c \'echo $UID\''), '^%s*(.-)%s*$', '%1') == '1027'
 -- VIM_CONFIG_FILE = resolve(expand($MYVIMRC))
