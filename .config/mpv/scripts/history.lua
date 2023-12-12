87292e2f7e14f3c37ea5b33c6a2f53b339b5f5f4
Andrew Grechkin <andrew.grechkin@gmail.com>
2023-12-12 08:45:53 +0100

    update scripts and configs

--- .config/mpv/scripts/history.lua
+++ .config/mpv/scripts/history.lua
@@ -6,34 +6,34 @@ local HISTFILE = os.getenv('XDG_CACHE_HOME') .. '/mpv/history.log';
 
 mp.add_hook('on_load', 50, function()
     local path = mp.get_property_native('path')
+    if not path then return end
 
-    if true then
+    if string.sub(path, 1, 1) ~= '/' then
         local cwd = mp.get_property_native('working-directory')
         path = utils.join_path(cwd, path)
     end
 
     local stat = utils.file_info(path)
+    if not stat then return end
+
     msg.debug(('path: %s, stat: %s'):format(path, utils.format_json(stat)))
 
     if stat.is_dir then
         local _, basename = utils.split_path(path)
-        local options = {}
-        if mp.get_property_native('osc') == false then table.insert(options, '--no-osc') end
-        if mp.get_property_native('term-osd') == 'force' then table.insert(options, '--term-osd=force') end
-        if mp.get_property_native('audio-display') == false then table.insert(options, '--no-audio-display') end
+        local opt = {}
+        if mp.get_property_native('osc') == false then table.insert(opt, '--no-osc') end
+        if mp.get_property_native('term-osd') == 'force' then table.insert(opt, '--term-osd=force') end
+        if mp.get_property_native('audio-display') == false then table.insert(opt, '--no-audio-display') end
 
         local fp = io.open(HISTFILE, 'a+');
         if fp then
-            fp:write(('[%s]\t%s\tmpv %s "%s"\n'):format(os.date('%Y-%m-%dT%X'), basename, table.concat(options, ' '),
-                path));
+            fp:write(('[%s]\t%s\tmpv %s "%s"\n'):format(os.date('%Y-%m-%dT%X'), basename, table.concat(opt, ' '), path));
             fp:close();
         end
     end
 end)
 
 mp.register_event('file-loaded', function()
-    local title, fp;
-
     local video_format = mp.get_property('video-format')
     if video_format and video_format ~= 'mjpeg' then
         print(video_format)
@@ -41,11 +41,11 @@ mp.register_event('file-loaded', function()
         return
     end
 
-    title = mp.get_property('media-title');
+    local title = mp.get_property('media-title');
     title = (title == mp.get_property('filename') and '' or (' (%s)'):format(title));
     if title == '' then title = mp.get_property('path') end
 
-    fp = io.open(HISTFILE, 'a+');
+    local fp = io.open(HISTFILE, 'a+');
     if fp then
         fp:write(('[%s]\t%s\tmpv "%s"\n'):format(os.date('%Y-%m-%dT%X'), title, mp.get_property('path')));
         fp:close();
