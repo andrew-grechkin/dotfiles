bf1b016627775ef454f788961a09989ccfaadece
Andrew Grechkin <andrew.grechkin@gmail.com>
2023-12-13 08:12:32 +0100

    update scripts and configs

--- .config/mpv/scripts/blacklist-extensions.lua
+++ .config/mpv/scripts/blacklist-extensions.lua
@@ -1,78 +1,62 @@
-opts = {
-    blacklist="jpg,png",
-    whitelist="",
+local mp = require 'mp'
+local options = require 'mp.options'
+local msg = require 'mp.msg'
+
+OPTS = {
+    blacklist = 'gif,jpg,png',
+    whitelist = '',
     remove_files_without_extension = false,
     oneshot = true,
 }
-(require 'mp.options').read_options(opts)
-local msg = require 'mp.msg'
 
-function split(input)
+options.read_options(OPTS)
+
+local split = function(input)
     local ret = {}
-    for str in string.gmatch(input, "([^,]+)") do
-        ret[#ret + 1] = str
-    end
+    for str in string.gmatch(input, '([^,]+)') do ret[#ret + 1] = str end
     return ret
 end
 
-opts.blacklist = split(opts.blacklist)
-opts.whitelist = split(opts.whitelist)
+OPTS.blacklist = split(OPTS.blacklist)
+OPTS.whitelist = split(OPTS.whitelist)
 
 local exclude
-if #opts.whitelist > 0 then
+if #OPTS.whitelist > 0 then
     exclude = function(extension)
-        for _, ext in pairs(opts.whitelist) do
-            if extension == ext then
-                return false
-            end
-        end
+        for _, ext in pairs(OPTS.whitelist) do if extension == ext then return false end end
         return true
     end
-elseif #opts.blacklist > 0 then
+elseif #OPTS.blacklist > 0 then
     exclude = function(extension)
-        for _, ext in pairs(opts.blacklist) do
-            if extension == ext then
-                return true
-            end
-        end
+        for _, ext in pairs(OPTS.blacklist) do if extension == ext then return true end end
         return false
     end
 else
     return
 end
 
-function should_remove(filename)
-    if string.find(filename, "://") then
-        return false
-    end
-    local extension = string.match(filename, "%.([^./]+)$")
-    if not extension and opts.remove_files_without_extension then
-        return true
-    end
-    if extension and exclude(string.lower(extension)) then
-        return true
-    end
+local should_remove = function(filename)
+    if string.find(filename, '://') then return false end
+    local extension = string.match(filename, '%.([^./]+)$')
+    if not extension and OPTS.remove_files_without_extension then return true end
+    if extension and exclude(string.lower(extension)) then return true end
     return false
 end
 
-function process(playlist_count)
+local process = function(playlist_count)
     if playlist_count < 2 then return end
-    if opts.oneshot then
-        mp.unobserve_property(observe)
-    end
-    local playlist = mp.get_property_native("playlist")
+    if OPTS.oneshot then mp.unobserve_property(OBSERVE) end
+    local playlist = mp.get_property_native('playlist')
     local removed = 0
     for i = #playlist, 1, -1 do
         if should_remove(playlist[i].filename) then
-            mp.commandv("playlist-remove", i-1)
+            mp.commandv('playlist-remove', i - 1)
             removed = removed + 1
         end
     end
-    if removed == #playlist then
-        msg.warn("Removed eveything from the playlist")
-    end
+    if removed == #playlist then msg.warn('Removed eveything from the playlist') end
 end
 
-function observe(k,v) process(v) end
+OBSERVE = function(_, v) process(v) end
 
-mp.observe_property("playlist-count", "number", observe)
+mp.observe_property('playlist-count', 'number', OBSERVE)
--- .config/mpv/scripts/history.lua
+++ .config/mpv/scripts/history.lua
@@ -1,53 +1,53 @@
 local mp = require 'mp'
-local msg = require 'mp.msg'
+local log = require 'mp.msg'
 local utils = require 'mp.utils'
 
 local HISTFILE = os.getenv('XDG_CACHE_HOME') .. '/mpv/history.log';
 
-mp.add_hook('on_load', 50, function()
+local add_to_history = function(title, path)
+    local opt = {}
+    if mp.get_property_native('osc') == false then table.insert(opt, '--no-osc') end
+    if mp.get_property_native('term-osd') == 'force' then table.insert(opt, '--term-osd=force') end
+    if mp.get_property_native('audio-display') == false then table.insert(opt, '--no-audio-display') end
+
+    local fp = io.open(HISTFILE, 'a+');
+    if fp then
+        fp:write(('[%s]\t%s\tmpv %s "%s"\n'):format(os.date('%Y-%m-%dT%X'), title, table.concat(opt, ' '), path));
+        fp:close();
+    end
+end
+
+mp.add_hook('on_load', 50, function(hook)
     local path = mp.get_property_native('path')
+    log.info(('on_load: "%s" %s'):format(path, utils.to_string(hook)))
     if not path then return end
 
-    if string.sub(path, 1, 1) ~= '/' then
-        local cwd = mp.get_property_native('working-directory')
-        path = utils.join_path(cwd, path)
-    end
+    local cwd = mp.get_property_native('working-directory')
+    path = utils.join_path(cwd, path)
 
     local stat = utils.file_info(path)
     if not stat then return end
 
-    msg.debug(('path: %s, stat: %s'):format(path, utils.format_json(stat)))
+    log.info(('path: %s, stat: %s'):format(path, utils.to_string(stat)))
 
-    if stat.is_dir then
-        local _, basename = utils.split_path(path)
-        local opt = {}
-        if mp.get_property_native('osc') == false then table.insert(opt, '--no-osc') end
-        if mp.get_property_native('term-osd') == 'force' then table.insert(opt, '--term-osd=force') end
-        if mp.get_property_native('audio-display') == false then table.insert(opt, '--no-audio-display') end
+    if not stat.is_dir then return end
 
-        local fp = io.open(HISTFILE, 'a+');
-        if fp then
-            fp:write(('[%s]\t%s\tmpv %s "%s"\n'):format(os.date('%Y-%m-%dT%X'), basename, table.concat(opt, ' '), path));
-            fp:close();
-        end
-    end
+    local _, basename = utils.split_path(path)
+    add_to_history(basename, path)
 end)
 
-mp.register_event('file-loaded', function()
+local ignore = {gif = 1, png = 1, mjpg = 1, mjpeg = 1, jpeg = 1, jpg = 1}
+
+mp.register_event('file-loaded', function(event)
+    local path = mp.get_property('path')
     local video_format = mp.get_property('video-format')
-    if video_format and video_format ~= 'mjpeg' then
-        print(video_format)
-    else
-        return
-    end
+    log.info(('%s: "%s" %s'):format(utils.to_string(event), path, video_format))
+
+    if not video_format or ignore[video_format] ~= nil then return end
 
     local title = mp.get_property('media-title');
     title = (title == mp.get_property('filename') and '' or (' (%s)'):format(title));
-    if title == '' then title = mp.get_property('path') end
+    if title == '' then title = path end
 
-    local fp = io.open(HISTFILE, 'a+');
-    if fp then
-        fp:write(('[%s]\t%s\tmpv "%s"\n'):format(os.date('%Y-%m-%dT%X'), title, mp.get_property('path')));
-        fp:close();
-    end
+    add_to_history(title, path)
 end)
