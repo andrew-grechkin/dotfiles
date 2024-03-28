328b932b88e555d04c3eb6ba5ac085b056619f83
Andrew Grechkin <andrew.grechkin@gmail.com>
2024-03-28 09:03:26 +0100

    update scripts and configs

--- .config/zsh/lib/distrobox/distrobox.plugin.zsh
+++ .config/zsh/lib/distrobox/distrobox.plugin.zsh
@@ -8,9 +8,5 @@ alias dib-images='distrobox --compatibility'
 # => main --------------------------------------------------------------------------------------------------------- {{{1
 
 function dib-create-arch () {
-	if [[ -r "$HOME/git/private/dotfiles/.local/share/distrobox/resolv.conf" ]]; then
-		distrobox-assemble create --file "$HOME/.local/share/distrobox/distrobox-work.ini" -n arch
-	else
-		distrobox-assemble create --file "$HOME/.local/share/distrobox/distrobox.ini" -n arch
-	fi
+	distrobox-assemble create --file "$HOME/.local/share/distrobox/distrobox.ini" -n arch
 }
