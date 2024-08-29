1ece038dab4ebc5979684c35ac47e64d6cd57138
Andrei Grechkin <andrei.grechkin@booking.com>
2024-08-29 10:44:19 +0200

    chore: misc updates

--- .config/zsh/lib/distrobox/distrobox.plugin.zsh
+++ .config/zsh/lib/distrobox/distrobox.plugin.zsh
@@ -8,5 +8,6 @@ alias dib-images='distrobox --compatibility'
 # => main --------------------------------------------------------------------------------------------------------- {{{1
 
 function dib-create-arch () {
+	docker build --tag=quay.io/toolbx-images/archlinux-toolbox:latest --file="$HOME/.local/share/distrobox/Dockerfile-arch" .
 	distrobox-assemble create --file "$HOME/.local/share/distrobox/distrobox.ini" -n arch
 }
