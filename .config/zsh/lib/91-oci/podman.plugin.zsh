0ccb7135e2ea1c6b4032e95c6a89fe193773cbf5
Andrei Grechkin <andrei.grechkin@booking.com>
2024-10-09 16:16:52 +0200

    chore: reorg zsh lib

--- .config/zsh/lib/91-oci/podman.plugin.zsh
+++ .config/zsh/lib/91-oci/podman.plugin.zsh
@@ -2,7 +2,7 @@
 
 # => exports ------------------------------------------------------------------------------------------------------ {{{1
 
-if [[ -x "$(command -v podman)" ]] && [[ -z "$DOCKER_HOST" ]] && [[ -S "${XDG_RUNTIME_DIR}/podman/podman.sock" ]]; then
+if [[ -x "$(command -v podman)" || -x "$(command -v docker)" ]] && [[ -z "$DOCKER_HOST" ]] && [[ -S "${XDG_RUNTIME_DIR}/podman/podman.sock" ]]; then
 	export DOCKER_HOST="unix://${XDG_RUNTIME_DIR}/podman/podman.sock"
 fi
 
