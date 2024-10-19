68b86207b3325c3a3837f90ee3cab3b1f8f21837
Andrew Grechkin <andrew.grechkin@gmail.com>
2024-10-19 12:36:17 +0200

    chore: misc updates

--- .config/nvim/lua/plugins/90-markdown.lua
+++ .config/nvim/lua/plugins/90-markdown.lua
@@ -1,10 +1,11 @@
 return {
-    { -- https://github.com/iamcco/markdown-preview.nvim
-        'iamcco/markdown-preview.nvim',
-        enabled = not IS_WORK or not IS_KVM,
-        build = 'cd app && yarn install',
-        cmd = {'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop'},
-        ft = {'markdown'},
-        init = function() vim.g.mkdp_filetypes = {'markdown'} end,
-    },
+    -- reqires yarn
+    -- { -- https://github.com/iamcco/markdown-preview.nvim
+    --     'iamcco/markdown-preview.nvim',
+    --     enabled = not IS_WORK or not IS_KVM,
+    --     build = 'cd app && yarn install',
+    --     cmd = {'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop'},
+    --     ft = {'markdown'},
+    --     init = function() vim.g.mkdp_filetypes = {'markdown'} end,
+    -- },
 }
