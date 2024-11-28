45ecda69d1bd219dab869a85daa136d1041feaaf
Andrew Grechkin <andrew.grechkin@gmail.com>
2024-11-28 17:40:07 +0100

    chore: misc updates

--- .local/lib/shell/gitlab.bash
+++ .local/lib/shell/gitlab.bash
@@ -1,34 +1,6 @@
 # vim: filetype=sh
 # shellcheck disable=SC2034
 
-function url_encode() {
-	printf %s "$1" | jq -Rr @uri
-}
-
-function join() {
-	local IFS=$'\t';
-	echo -e "$*"
-}
-
-function json-array-to-tsv() {
-	sort_by="$1"
-	shift
-	jq_fields=("$@")
-
-	col_names=()
-	jq_filter=()
-	for field in "${jq_fields[@]}"; do
-		IFS=";" read -r -a keyval <<< "$field"
-		col_names+=("${keyval[0]}")
-		jq_filter+=("${keyval[1]}")
-	done
-
-	{
-		echo -e "$(join "${col_names[@]}")"
-		jq -r "sort_by($sort_by) | reverse | .[] | \"$(join "${jq_filter[@]}")\""
-	} | tsv-align
-}
-
 if [[ -t 1 ]]; then
 	INTERACTIVE=1
 	PRINT_HEADER=1
