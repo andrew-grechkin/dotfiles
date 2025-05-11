976ae33ce016f8747cee8815dd967ee9efcfe7dc
Andrew Grechkin <andrew.grechkin@gmail.com>
2025-05-11 23:04:56 +0200

    chore: misc updates

--- .config/zsh/lib/90-develop/python.plugin.zsh
+++ .config/zsh/lib/90-develop/python.plugin.zsh
@@ -2,7 +2,7 @@
 
 # => exports ------------------------------------------------------------------------------------------------------ {{{1
 
-export PYENV_ROOT="${PYENV_ROOT:-$XDG_DATA_HOME/pyenv}"
+# export PYENV_ROOT="${PYENV_ROOT:-$XDG_DATA_HOME/pyenv}"
 
 # => aliases ------------------------------------------------------------------------------------------------------ {{{1
 
@@ -11,21 +11,21 @@ export PYENV_ROOT="${PYENV_ROOT:-$XDG_DATA_HOME/pyenv}"
 
 # => functions ---------------------------------------------------------------------------------------------------- {{{1
 
-function activate-local-python() {
-	if (( $+commands[poetry] )); then
-		local PYTHON_LOCAL="$(poetry env info -p)"
-	else                                                                           # fallback onto venv if poetry is not available
-		local PYTHON_LOCAL=${1:-${XDG_CACHE_HOME}/python3}
-		[[ ! -f "$PYTHON_LOCAL/bin/activate" ]] && python3 -m venv "$PYTHON_LOCAL"
-	fi
+# function activate-local-python() {
+# 	if (( $+commands[poetry] )); then
+# 		local PYTHON_LOCAL="$(poetry env info -p)"
+# 	else                                                                           # fallback onto venv if poetry is not available
+# 		local PYTHON_LOCAL=${1:-${XDG_CACHE_HOME}/python3}
+# 		[[ ! -f "$PYTHON_LOCAL/bin/activate" ]] && python3 -m venv "$PYTHON_LOCAL"
+# 	fi
 
-	source-file "$PYTHON_LOCAL/bin/activate"
-}
+# 	source-file "$PYTHON_LOCAL/bin/activate"
+# }
 
-function enable-pyenv() {
-	export PATH="$PYENV_ROOT/bin:$PATH"
-	eval "$(pyenv init -)"
-}
+# function enable-pyenv() {
+# 	export PATH="$PYENV_ROOT/bin:$PATH"
+# 	eval "$(pyenv init -)"
+# }
 
 # => main --------------------------------------------------------------------------------------------------------- {{{1
 
