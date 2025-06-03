277634ddb3b9e751d1763bed0050fd57b9409dcb
Andrew Grechkin <andrew.grechkin@gmail.com>
2025-06-03 23:39:59 +0200

    chore: misc updates

--- .local/lib/shell/rsync.bash
+++ .local/lib/shell/rsync.bash
@@ -21,11 +21,19 @@ RSYNC_MOVE_PARAMS=(
 )
 
 RSYNC_NAS_PARAMS_COMMON=(
-	--info=progress2
 	--no-group
 	--no-owner
 	--omit-dir-times
 	--rsh=ssh
+)
+
+RSYNC_INFO_TTY=(
+	--info=progress2
+)
+
+RSYNC_INFO_FILE=(
+	-i
+	--info=stats
 )
 
 RSYNC_DIFF_PARAMS_COMMON=(
