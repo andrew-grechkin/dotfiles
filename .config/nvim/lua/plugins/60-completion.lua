36fd252a46856b15b8102f6cd675dc9759a3d8cd
Andrew Grechkin <andrew.grechkin@gmail.com>
2025-09-23 17:10:40 +0200

    fix(neovim): update configs

--- .config/nvim/lua/plugins/60-completion.lua
+++ .config/nvim/lua/plugins/60-completion.lua
@@ -1,4 +1,15 @@
 return {
+    {
+        'folke/lazydev.nvim',
+        ft = 'lua', -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                {path = '${3rd}/luv/library', words = {'vim%.uv'}},
+            },
+        },
+    },
     { -- https://github.com/hrsh7th/nvim-cmp
         'hrsh7th/nvim-cmp',
         dependencies = {
