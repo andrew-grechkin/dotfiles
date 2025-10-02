ee955f774342bb15d9cd3a62240d8034f9921e1c
Andrew Grechkin <andrew.grechkin@gmail.com>
2025-10-02 08:14:40 +0200

    refactor: fzf infrastructure

---
+++ .config/zsh/lib/99-theme/jq.theme.zsh
@@ -0,0 +1,5 @@
+# https://github.com/jonschlinkert/sublime-monokai-extended/blob/master/Monokai%20Extended.tmTheme
+
+# from grand-molokai
+#                "  null  : false  :  true  : number : string : array  : object :   key  "
+export JQ_COLORS="38;5;117:38;5;135:38;5;141:38;5;109:38;5;143:38;5;245:38;5;245:38;5;208"
