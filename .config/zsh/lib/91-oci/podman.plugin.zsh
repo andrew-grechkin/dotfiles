8414330e7afb1425537ea540df3f415179aa5d9d
Andrew Grechkin <andrew.grechkin@gmail.com>
2026-05-15 20:24:54 +0200

    feat(scripts): add `kube` tool to manage local kubernetes cluster

--- .config/zsh/lib/91-oci/podman.plugin.zsh
+++ .config/zsh/lib/91-oci/podman.plugin.zsh
@@ -2,8 +2,12 @@
 
 # => exports ------------------------------------------------------------------------------------------------------ {{{1
 
-if [[ -x "$(command -v podman)" || -x "$(command -v docker)" ]] && [[ -z "$DOCKER_HOST" ]] && [[ -S "${XDG_RUNTIME_DIR}/podman/podman.sock" ]]; then
-	export DOCKER_HOST="unix://${XDG_RUNTIME_DIR}/podman/podman.sock"
+if [[ -S "${XDG_RUNTIME_DIR}/podman/podman.sock" ]]; then
+    if [[ -x "$(command -v podman)" || -x "$(command -v docker)" ]]; then
+       [[ -z "${CONTAINER_HOST:-}" ]] && export CONTAINER_HOST="unix://${XDG_RUNTIME_DIR}/podman/podman.sock"
+       [[ -z "${DOCKER_HOST:-}" ]] && export DOCKER_HOST="unix://${XDG_RUNTIME_DIR}/podman/podman.sock"
+       [[ -z "${DOCKER_SOCK:-}" ]] && export DOCKER_SOCK="${XDG_RUNTIME_DIR}/podman/podman.sock"
+    fi
 fi
 
 # => main --------------------------------------------------------------------------------------------------------- {{{1
